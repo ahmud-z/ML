d8de3b0e25fee27038022db9453c4a9afaba447d
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-11-14 18:03:29 +0600

    simple sentiment analysis: dosent work very well

--- simple_sentiment_analysis/main.py
+++ simple_sentiment_analysis/main.py
@@ -5,7 +5,12 @@ text = input("Enter your text or review: ")
 blob = TextBlob(text)
 sentiment = blob.sentiment.polarity
 
-if sentiment >= 0:
-    print("Positive Review")
+print(f"Score: {sentiment}")
+
+if sentiment > 0:
+    print("positive")
+elif sentiment < 0:
+    print("negative")
 else:
-    print("Negative Review")
+    print("neutral")
+
